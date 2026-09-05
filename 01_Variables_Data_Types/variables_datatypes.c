3087829c0af6ad149a2f2bd22bc263fadf3f1e6f
kalyanbandaru734 <kalyanbandaru126@gmail.com>
2026-09-05 17:33:56 +0530

    Add files via upload

---
+++ 01_Variables_Data_Types/variables_datatypes.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int age = 20;
+    float voltage = 3.3f;
+    char grade = 'A';
+
+    printf("Integer: %d\n", age);
+    printf("Float: %.2f V\n", voltage);
+    printf("Character: %c\n", grade);
+
+    return 0;
+}

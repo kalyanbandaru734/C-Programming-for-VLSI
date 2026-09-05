0ee583c090aaa150f297179f7954ca1ab2458b13
kalyanbandaru734 <kalyanbandaru126@gmail.com>
2026-09-05 17:35:29 +0530

    Add files via upload

---
+++ 02_If_Else/if_else.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int voltage = 5;
+
+    if (voltage > 3)
+        printf("Voltage is above 3V\n");
+    else
+        printf("Voltage is 3V or below\n");
+
+    return 0;
+}
